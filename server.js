01cebaaa2a974ca46da08365294b6693c4aaf12c
RohitYewale880 <rohityewale2020@gmail.com>
2026-07-06 01:41:37 +0530

    cors

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const BolgRoutes = require('./routes/routes_blog');
 
 const app = express();
 app.use(cors({
-    origin : ['http://localhost:4200'],
+    origin : ['https://blog-frontend-alpha-liard.vercel.app/', 'http://localhost:4200'],
     methods : ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 }));
 app.use(express.json())
 
 app.use("/api/blogs", BolgRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server runs on post ${PORT}`);
